af7801902329057bd86673c010de572fc5637e21
NoetKevin <kevin.noet@soprasteria.com>
2022-08-10 17:44:46 +0200

    Add importlib

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -7,13 +7,13 @@ from pathlib import Path
 
 def load_module(self, directory, name):
     sys.path.insert(0, directory)
-    import_module(name)
+    importlib.import_module(name)
     sys.path.pop(0)
 
 def load_directory(self, directory):
-    for path in Path(directory).rglob(".py"):
+    for path in directory.rglob("*.py"):
         load_module(directory.as_posix(), path.stem)
 
 def load_bundled(self):
     directory= Path(__file__).parent / "extensions"
-    self.load_directory(directory)
+    load_directory(directory)
