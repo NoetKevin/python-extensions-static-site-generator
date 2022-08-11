c3293bfeeed502fb9a4dbf696efa4e39d5345ca4
NoetKevin <kevin.noet@soprasteria.com>
2022-08-11 09:15:51 +0200

    Stats Extension

---
+++ ssg/extensions/stats.py
@@ -0,0 +1,26 @@
+import time
+
+
+from ssg import hooks
+
+
+start_time = None
+total_written = 0
+
+@stats.register("start_build")
+def start_build():
+    global start_time = time.time()
+
+
+@stats.register("written")
+def written():
+    global total_written += 1
+
+@stats.register("stats")
+def stats():
+    global start_time
+    final_time = time.time() - start_time
+    average = if total_written != 0 : final_time / total_written
+    report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
+    print (report.format(total_written, final_time, average))
+    
