ab1a16bb71d43cc6c31a21bfc038e72619fed27a
NoetKevin <kevin.noet@soprasteria.com>
2022-08-11 09:42:07 +0200

    Oui

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -10,13 +10,13 @@ total_written = 0
 @hooks.register("start_build")
 def start_build():
     global start_time
-    time = time.time()
+    start_time = time.time()
 
 
 @hooks.register("written")
 def written():
     global total_written
-    total_written += 1
+    total_written = total_written + 1
 
 @hooks.register("stats")
 def stats():
